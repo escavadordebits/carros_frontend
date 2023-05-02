078fb93a881eb8155b578463d97ff5f07efc1b6a
PauloAndre <paulo.moraes@evo.com.br>
2023-05-02 12:37:51 -0300

    ComitFinal

--- js/main.js
+++ js/main.js
@@ -15,22 +15,22 @@ function getcarro() {
         if (!userinput) {
             var list = data.Carros;
             var dados;
-            
+
             for (var i = 0; i < list.length; i++) {
 
                 dados = list[i];
                 oDatset.item.push(dados);
 
                 var tr = document.createElement('tr');
-             
+
                 var tdModelo = document.createElement('td');
                 var tdAno = document.createElement('td');
                 var tdValor = document.createElement('td');
-            
+
                 tdAno.textContent = oDatset.item[i].ano;
                 tdModelo.textContent = oDatset.item[i].modelo;
                 tdValor.textContent = oDatset.item[i].valor;
-          
+
                 tr.appendChild(tdAno);
                 tr.appendChild(tdModelo);
                 tr.appendChild(tdValor);
@@ -38,11 +38,11 @@ function getcarro() {
 
             }
         } else {
-          
+
             document.getElementById("ano").value = data.ano;
             document.getElementById("valor").value = data.valor;
             document.getElementById("carro_id").value = data.id;
-           
+
         }
 
 
@@ -82,12 +82,13 @@ function getcarro() {
 
 };
 function salvarcarrocoment() {
-    debugger
+
     var userinputcomment = document.getElementById("comentario").value;
     var formData = {
-        carro_id: $("#carro_id").val(),
-        comentario: $("#comentario").val()
-      };
+        carro_id: parseInt($("#carro_id").val()),
+        texto: $("#comentario").val()
+    };
+    debugger
     if (userinputcomment || userinputcomment.length > 0) {
         $.ajax({
             url: "http://127.0.0.1:5000/comentario",
@@ -113,29 +114,29 @@ function salvarcarrocoment() {
 };
 $(document).ready(function () {
     $("form").submit(function (event) {
-      var formData = {
-        ano: $("#ano").val(),
-        modelo: $("#modelo").val(),
-        valor: $("#valor").val(),
-      };
-  debugger
-      $.ajax({
-        type: "POST",
-        url: "http://127.0.0.1:5000/carro",
-        data: formData,
-        dataType: "json",
-        encode: true,
-        error:function(){
-            alert('Erro de cadastro');
-        },
-      }).done(function () {
-        alert('Cadastrado com Sucesso');
-        window.location.reload();
-      });
-  
-      event.preventDefault();
+        var formData = {
+            ano: $("#ano").val(),
+            modelo: $("#modelo").val(),
+            valor: $("#valor").val(),
+        };
+        debugger
+        $.ajax({
+            type: "POST",
+            url: "http://127.0.0.1:5000/carro",
+            data: formData,
+            dataType: "json",
+            encode: true,
+            error: function () {
+                alert('Erro de cadastro');
+            },
+        }).done(function () {
+            alert('Cadastrado com Sucesso');
+            window.location.reload();
+        });
+
+        event.preventDefault();
     });
-  });
+});
 
 
 function deletecarro() {
